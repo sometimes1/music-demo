1cab722810a855e76ade5f3777f7378c9e606427
小月月 <pengyufang520@qq.com>
2017-08-30 03:07:07 +0800

    完成搜索功能 搜索历史还没弄

--- scripts/home.js
+++ scripts/home.js
@@ -18,11 +18,13 @@ query.find().then(function (results) {
     for (var i = 0; i < results.length; i++) {
         var song = results[i].attributes
         var li = `
-                        <li>
-                            <h3>${song.name}</h3>
-                            <p><i></i>${song.singer}</p>
-                            <a href="${song.url}"></a>
-                        </li>
+        <li>
+        <a href="${song.url}">
+            <h3>${song.name}</h3>
+            <p><i></i>${song.singer}</p>
+            <div></div>
+        </a>
+        </li>
                 `
         $olSongs.append(li)
 
@@ -47,14 +49,15 @@ $('input#search').on('input', function (e) {
         query.find().then(function (results) {
             $('#searchResult').empty()
             if(results.length === 0){
-                $('#searchResult').html('暂无搜索结果')
+                $('#searchResult').html('<div>暂无搜索结果</div>')
+                $('.search-content').hide()
             }else{
                 for (var i = 0; i < results.length; i++) {
                     var song = results[i].attributes
                     var li = `
                 <li>
                         <i class="iconfont icon-weibiaoti-"></i>
-                        <a href="./song.html?id=${results[i].id}">${song.name}</a>
+                        <a href="#}">${song.name}</a>
                 </li>
                 `
                     $('#searchResult').append(li)
@@ -63,7 +66,6 @@ $('input#search').on('input', function (e) {
                     return (close())
                 }
             }
-            
         })
     }, 400)
 })
@@ -83,10 +85,66 @@ $('.close').on('click', function () {
 
 
 /*热门搜索*/
-$('.hot-seek > ul').on('click', 'li', function () {
-    $('#search').val($(this).text())
-    hot()
+$('.hot-seek > ul').on('click', 'li', function (e) {
+    var $ul = $(e.currentTarget)
+    $('#search').val($ul.text())
+    var value = $('#search').val()
+    var query = new AV.Query('Song');
+    $('.hot-seek').hide()
+    $('.close').addClass('active')
+    $('.search-content').html(`<h3>搜索“${value}”</h3>`).show()
+    query.contains('name', value);
+    query.find().then(function (results){
+        $('#searchResult').empty()
+        if(results.length === 0){
+            $('#searchResult').html('<div>暂无搜索结果</div>')
+            $('.search-content').hide()
+        }else{
+            for (var i = 0; i < results.length; i++) {
+                var song = results[i].attributes
+                var li = `
+            <li>
+                    <i class="iconfont icon-weibiaoti-"></i>
+                    <a href="#">${song.name}</a>
+            </li>
+            `
+                $('#searchResult').append(li)
+            }
+            if (value === '') {
+                return (close())
+            }
+        }
+    })
 })
+//显示搜索
+$('#searchResult').on('click','li > a',function(e){
+    $a = $(e.currentTarget)
+    var value = $a.text()
+    var query = new AV.Query('Song');
+    $('.search-content').hide()
+    query.contains('name', value);
+    query.find().then(function (results){
+        $('#searchResult').empty()
+        for (var i = 0; i < results.length; i++) {
+            var song = results[i].attributes
+            var ol = `
+            <ol class="list" id="songs">
+            <li>
+            <a href="${song.url}">
+                <h3>${song.name}</h3>
+                <p><i></i>${song.singer}</p>
+                <div></div>
+            </a>
+            </li>
+            </ol>
+    `
+            $('#searchResult').append(ol)
+        }
+    })
+})
+
+
+
 
 /*热门歌曲列表*/
 
@@ -94,6 +152,8 @@ $('.hotlist > ul').on('click', 'li>div>span', function () {
     $('#search').val($(this).text())
     hot()
 })
+
+//搜索包装
 var hot = function () {
     $('.hotlist').hide()
     $('.hot-seek').hide()
@@ -103,6 +163,7 @@ var hot = function () {
     $('.search-content').html(`<h3>搜索“${value}”</h3>`).show()
 }
 
+ //
 
         /*歌曲上传
         var SongObject = AV.Object.extend('Song');//选择表名
